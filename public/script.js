318cee356b8a6a503d46e293476212022810fe4d
Emmanuel Armstrong <emmanuelrarmstrong@gmail.com>
2015-10-05 21:10:08 -0600

    Finished the homework

--- public/script.js
+++ public/script.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     // get all of our list items
     // 1. Select all the items to filter
-    var dataList =
+    var dataList = document.createElement("select");
+    dataList.id = "mySelect";
+    myDiv.appendChild(dataList);
     //setup click event handlers on our checkboxes
     // 2. Select all the checkboxes to watch
-    var checkBoxes =
+    var checkBoxes = document.createElement('checkBoxes');
+    var val = 1;
+    var cap;
+    checkBoxes.type = 'checkbox';
+    checkBoxes.name = val;
+    checkBoxes.value = cap;
+    checkBoxes.appendChild(document.createTextNode(cap));
 
     for (var i = 0; i < checkBoxes.length; i++) {
         checkBoxes[i].checked = true;
+        document.addEventListener('DOMContentLoaded', filterItems(){
+            document.querySelector('checkBox1'.addEventListener('checkBoxStateChange', Handler);
+        });
         // 3. Add an event listener for each checkbox
         // each checkbox should use filterItems()
 
@@ -24,8 +35,8 @@ document.addEventListener("DOMContentLoaded", function() {
         for (var i = 0; i < dataList.length; i++) {
             var item = dataList[i];
             // 4. show or hide the item if the item attribute "data-category" == value
-            if() {
-                if(show){
+            if(item == value) {
+                if(filterItems() == true){
                     item.style.display = "block";
                 }else{
                     item.style.display = "none";
